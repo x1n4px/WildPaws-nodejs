0b557178b5715e7ab5f7b409b293acc47ba2de92
x1n4px <silosabia@gmail.com>
2023-09-10 14:09:59 +0200

    act

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const PORT = process.env.PORT || 3000
 
-export const DB_HOST = process.eventNames.DB_HOST || 'localhost'
+export const DB_HOST = process.env.DB_HOST || 'localhost'
 
 export const DB_USER = process.env.DB_USER || 'libreriauser'
 
